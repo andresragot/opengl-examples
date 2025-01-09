9207fae898926326a41995ca31774ba60fb2b7a4
Andrés Ragot <andresragot99@gmail.com>
2025-01-09 23:53:07 +0100

    blinn-phong lightning

---
+++ 3 blinn-phong lighting/projects/xcode-16/geometry/geometry/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  geometry
+//
+//  Created by Andrés Ragot on 28/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
